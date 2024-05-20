dd2867bdebe5320a4f2bf13c432e60f6c54db06e
Nacho <nachofsloespinoza@gmail.com>
2024-05-20 16:00:20 -0400

    232w

--- crud-master/descargayt.php
+++ crud-master/descargayt.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="text" id="url" name="url"><br>
         <label for="formato">Seleccione el formato (mp4 o mp3):</label><br>
         <input type="text" id="formato" name="formato"><br><br>
-        <input type="submit" value="Descargar">
+        <button class="btn-success" name="Descargar">
     </form>
 </div>
 <?php include('includes/footer.php'); ?>
